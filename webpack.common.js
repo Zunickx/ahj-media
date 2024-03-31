bccc3c68c749e6fc9edf730834b72c2ec6f13eb2
Zunickx <Zunickx@gmail.com>
2024-03-31 17:56:37 +0300

    Create file README.md

---
+++ webpack.common.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+// Хеширование файлов только в production mode
+function addHash(fileName, buildMode, hash = 'contenthash') {
+  return buildMode === 'production' ? fileName.replace(/\.[^.]+$/, `.[${hash}]$&`) : fileName;
+}
+
+module.exports = (buildMode) => ({
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: addHash('[name].js', buildMode),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader, 'css-loader',
+        ],
+      },
+      {
+        test: /\.(svg|mp3|mp4)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 3072,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: addHash('[name].css', buildMode),
+      chunkFilename: addHash('[id].css', buildMode),
+    }),
+  ],
+});
